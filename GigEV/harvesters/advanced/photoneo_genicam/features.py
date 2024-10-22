835bdfe9f2e68d7923869ac21311f03671172e27
midhun.eldose <midhun.eldose>
2024-10-22 13:27:00 +0200

    added Line1 for hw_trigger

--- GigEV/harvesters/advanced/photoneo_genicam/features.py
+++ GigEV/harvesters/advanced/photoneo_genicam/features.py
@@ -4,7 +4,7 @@ from genicam.genapi import NodeMap
 def enable_trigger(features: NodeMap, source: str):
     features.TriggerSelector.value = "FrameStart"
     features.TriggerMode.value = "On"
-    features.TriggerSource.value = source
+    features.TriggerSource.value = "Line1"
 
 
 def enable_software_trigger(features: NodeMap):
