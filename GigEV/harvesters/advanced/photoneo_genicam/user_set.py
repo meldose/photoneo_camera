9576d726f8e1393494612a94f9db19b7645377a4
Midhun <meldose@gmail.com>
2024-10-24 22:26:24 +0200

    added comments in these files

--- GigEV/harvesters/advanced/photoneo_genicam/user_set.py
+++ GigEV/harvesters/advanced/photoneo_genicam/user_set.py
@@ -1,6 +1,6 @@
-from genicam.genapi import NodeMap
+from genicam.genapi import NodeMap # imported NodeMap module
 
 
-def load_default_user_set(features: NodeMap):
-    features.UserSetSelector.value = "Default"
-    features.UserSetLoad.execute()
+def load_default_user_set(features: NodeMap): # define the function called load_default_user_set
+    features.UserSetSelector.value = "Default" # set the value of UserSetSelector to Default
+    features.UserSetLoad.execute() # execute the UserSetLoad feature
--- GigEV/harvesters/advanced/photoneo_genicam/utils.py
+++ GigEV/harvesters/advanced/photoneo_genicam/utils.py
@@ -1,21 +1,21 @@
-import time
-from functools import wraps
+import time # imported time module
+from functools import wraps # imorted functools module
 
 
-def measure_time(func):
+def measure_time(func): # defined the fucnction measure_time
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start_time = time.time()
+        start_time = time.time() # assigning the value of the time.time() to start_time
         result = func(*args, **kwargs)
-        end_time = time.time()
-        execution_time_ms = (end_time - start_time) * 1000
+        end_time = time.time() # assigning the value of the time.time() to end_time
+        execution_time_ms = (end_time - start_time) * 1000 # execution_time_ms = end_time - start_time * 1000
         print(f"Execution time of {func.__name__}: {execution_time_ms:.2f} milliseconds")
         return result
 
-    return wrapper
+    return wrapper # returning the wrapper 
 
 
-def write_raw_array(filename: str, array):
+def write_raw_array(filename: str, array): # defined the function write_raw_array
     with open(filename, "wb") as file:
-        for element in array:
-            file.write(element.tobytes())
+        for element in array: # checking the element in the array
+            file.write(element.tobytes()) # write the element in the array
