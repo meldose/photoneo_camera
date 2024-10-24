1967b33a1d30254f32d1e45c58def67cafff79ee
Midhun <meldose@gmail.com>
2024-10-24 22:47:24 +0200

    added comments

--- GigEV/harvesters/advanced/hw_trigger.py
+++ GigEV/harvesters/advanced/hw_trigger.py
@@ -10,25 +10,25 @@ from photoneo_genicam.features import enable_hardware_trigger
 from photoneo_genicam.user_set import load_default_user_set
 
 
-def main(device_sn: str):
+def main(device_sn: str): # define the main function
     with Harvester() as h:
         h.add_file(str(producer_path), check_existence=True, check_validity=True)
-        h.update()
+        h.update() # update harvester
 
-        print(f"Connecting to: {device_sn}")
-        with h.create({"serial_number": device_sn}) as ia:
-            features: NodeMap = ia.remote_device.node_map
+        print(f"Connecting to: {device_sn}") # connect to device
+        with h.create({"serial_number": device_sn}) as ia: # connect to device with device_sn
+            features: NodeMap = ia.remote_device.node_map # get node map
 
             load_default_user_set(features)
             enable_hardware_trigger(features)
 
-            ia.start()
-            timeout = 180
+            ia.start() # start the device   
+            timeout = 180 # consider the timeout as 180
             print(f"Wait {timeout}s for hw-trigger signal...")
-            with ia.fetch(timeout=timeout) as buffer:
+            with ia.fetch(timeout=timeout) as buffer: # fetch the buffer with timeout
                 component_list: List[Component2DImage] = buffer.payload.components
-                for component in component_list:
-                    print(component)
+                for component in component_list: # conisder the components in component_list
+                    print(component) # print out the component
 
 
 if __name__ == "__main__":
@@ -37,15 +37,15 @@ if __name__ == "__main__":
         print(f"Device ID: {device_id}")  # Debug statement
     except IndexError:
         print("Error: no device given, please run it with the device serial number as argument:")
-        print(f"    {Path(__file__).name} <device serial>")
+        print(f"    {Path(__file__).name} <device serial>") # print the path file with device serial
         sys.exit(1)
 
     # Check if device_id is valid
-    if device_id is None:
-        print("Error: Device ID cannot be None.")
+    if device_id is None: # if the device_id is None
+        print("Error: Device ID cannot be None.") # print the error
         sys.exit(1)
 
-    main(device_id)
+    main(device_id)# calling the function main()
 
 
 
