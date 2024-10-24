d17ad0bdcc678cccf79ea8cbf596d5ed05ce5e7c
Midhun <meldose@gmail.com>
2024-10-24 21:41:41 +0200

    added comments

--- GigEV/harvesters/advanced/photoneo_genicam/features.py
+++ GigEV/harvesters/advanced/photoneo_genicam/features.py
@@ -1,15 +1,15 @@
-from genicam.genapi import NodeMap
+from genicam.genapi import NodeMap # immported the NodeMap module
 
 
-def enable_trigger(features: NodeMap, source: str):
-    features.TriggerSelector.value = "FrameStart"
-    features.TriggerMode.value = "On"
-    features.TriggerSource.value = "Line1"
+def enable_trigger(features: NodeMap, source: str): # defined an function called enabled trigger
+    features.TriggerSelector.value = "FrameStart" # consider TriggerSelector as FrameStart
+    features.TriggerMode.value = "On" # consider TriggerMode as On
+    features.TriggerSource.value = "Line1" # consider TriggerSource as Line1
 
 
 def enable_software_trigger(features: NodeMap):
-    enable_trigger(features, "Software")
+    enable_trigger(features, "Software") # enable software trigger
 
 
 def enable_hardware_trigger(features: NodeMap):
-    enable_trigger(features, "Line1")
+    enable_trigger(features, "Line1") # enable hardware trigger
