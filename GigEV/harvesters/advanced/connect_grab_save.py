5003ba8db6d40a5b781300d4d7c8684abed06d60
Midhun <meldose@gmail.com>
2024-10-27 10:56:58 +0100

    added comments in connect grave_save.py

--- GigEV/harvesters/advanced/connect_grab_save.py
+++ GigEV/harvesters/advanced/connect_grab_save.py
@@ -15,9 +15,9 @@ from photoneo_genicam.user_set import load_default_user_set
 from photoneo_genicam.utils import write_raw_array
 
 
-def process_component(component_name: str, component: Component2DImage):
-    pixel_format = component.data_format
-    if component_name in ("Intensity", "ColorCamera"):
+def process_component(component_name: str, component: Component2DImage): # deinded function process_component
+    pixel_format = component.data_format # assigning the value of data_format to pixel_format
+    if component_name in ("Intensity", "ColorCamera"): # if component is Intensity or ColorCamera
         component_data = component.data.copy()
         if pixel_format == "Mono10": # if pixel format is Mono10
             data = ((component_data.astype(np.uint16) << 6).reshape(component.height, component.width, 1))
@@ -37,46 +37,46 @@ def process_component(component_name: str, component: Component2DImage):
         write_raw_array(f"{component_name}_{pixel_format}.dat", component.data)
 
 
-def main(device_sn: str, *components: str):
+def main(device_sn: str, *components: str): # define the main function
 
     if len(components) == 0: # if length of components is 0
-        print("Warning: No component specified, using default: Range.")
-        components = ["Range"]
+        print("Warning: No component specified, using default: Range.") # print warning
+        components = ["Range"] # assigning the value of Range to components
 
     with Harvester() as h: # considet h as Harvester
-        h.add_file(str(producer_path), check_existence=True, check_validity=True)
+        h.add_file(str(producer_path), check_existence=True, check_validity=True) # add file to harvester
         h.update() # update harvester
 
         print(f"Connecting to: {device_sn}") # connect to device
         with h.create({"serial_number": device_sn}) as ia: # create ia with device_sn
             features: NodeMap = ia.remote_device.node_map
 
-            load_default_user_set(features)
-            enable_software_trigger(features)
+            load_default_user_set(features) # load default user set
+            enable_software_trigger(features) # enable software trigger
 
-            print(f"Requested component(s): {components}")
-            enable_components(features, list(components))
+            print(f"Requested component(s): {components}") # print out the components
+            enable_components(features, list(components)) # enable the components
             print(get_component_statuses(features, include_pixel_format=True))
 
-            ia.start()
-            features.TriggerSoftware.execute()
+            ia.start() # start the device
+            features.TriggerSoftware.execute() # execute the software trigger
             enabled_comps: list = enabled_components(features)
             with ia.fetch(timeout=10) as buff:
                 # match the components based on their order
-                for component_name, part in zip(enabled_comps, buff.payload.components):
-                    print(f"Saving component {component_name}")
+                for component_name, part in zip(enabled_comps, buff.payload.components): # checking the component_name and part in zip file
+                    print(f"Saving component {component_name}") # print out the component
                     print(f"  Data: {part}\n")
                     process_component(component_name, part)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # checking if the name of the file is main
     try:
         device_id = sys.argv[1]
         component_names = sys.argv[2:]
-    except IndexError:
+    except IndexError: # if the name of the file is not main
         print(
             "Error: no device or component given, please run it with the device serial number as argument:"
         )
         print(f"    {Path(__file__).name} <device serial> <component_name1> <component_name2> ...")
         sys.exit(1)
-    main(device_id, *component_names)
+    main(device_id, *component_names) # calling the main function with device_id and component_names
