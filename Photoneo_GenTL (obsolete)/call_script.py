41883e74a671199022042e068e2a771d0d3633f2
eskinjoseph <eskin.joseph@neura-robotics.com>
2024-11-21 14:44:18 +0100

    removed an print line

--- Photoneo_GenTL (obsolete)/call_script.py
+++ Photoneo_GenTL (obsolete)/call_script.py
@@ -10,9 +10,8 @@ def run_pointcloud_script():
     except subprocess.CalledProcessError as e:
         print(f"Error while executing the script: {e}")
     except FileNotFoundError:
-        print("The main script file 'main_script.py' was not found. Please ensure it is in the same directory.")
+        print("The main script file 'infinite_with_shaped_objects_detection.py' was not found. Please ensure it is in the same directory.")
 
 if __name__ == "__main__":
     print("Starting the point cloud and shape detection process...")
     run_pointcloud_script()
-    print("Process completed.")
